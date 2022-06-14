9db49c984045db213312a951a1341de4e5fcdaad
pioui <piyilozou@gmail.com>
2022-06-14 18:19:48 +0200

    package/envirioment fix

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,2 @@
-#from src import GKMI, normalize
-#from src import perform_classification
-
-#__all__ = [
-#'GKMI',
-#'normalize',
-#'perform_classification',
-#]
-
 #"""Top-level package."""
 from src import *
