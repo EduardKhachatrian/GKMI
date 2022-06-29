612e107eeee74237246c2c5c1cf596d988af4adb
pioui <piyilozou@gmail.com>
2022-06-29 14:06:33 +0200

    dkjnkjnb

--- scripts/pavia_config.py
+++ scripts/pavia_config.py
@@ -14,17 +14,15 @@ attribute_n   = 50
 n_clusters    = 'auto'
 segmentation_algorithm = 'slic'
 
-
-
-if not os.path.exists(data_dir):
-	print(f"Directory {data_dir} doesn't exist!")
-	logger.info(f"Directory {data_dir} doesn't exist!") 
-
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 logging.basicConfig(filename = os.path.join(output_dir,'gkmilogfile.log'))
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
+
+if not os.path.exists(data_dir):
+	print(f"Directory {data_dir} doesn't exist!")
+	logger.info(f"Directory {data_dir} doesn't exist!") 
 
 # TODO: add to the logger the configuration details
